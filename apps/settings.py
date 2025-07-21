8138f4eb60eb07b3eaf80f3643048ee4f0085041
Gunasekaran1106 <kugunasekaran171@gmail.com>
2025-07-21 14:24:20 +0530

    Update settings.py

--- apps/settings.py
+++ apps/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*h8!t^c!$3jy6!p$@av0y6)t+=&wm1deljc-dyt$!9()y3_)nc
 DEBUG = True
 
 #ALLOWED_HOSTS = ['local host','127.0.0.1','djangocrud-yits.onrender.com']
-ALLOWED_HOSTS = ['djangocrud-yits.onrender.com']
+ALLOWED_HOSTS = ['local host','127.0.0.1','djangocrud-yits.onrender.com']
 
 
 # Application definition
@@ -94,7 +94,7 @@ DATABASES = {
 #pip install pymysql (or) pip install mysqlclient
 
 
- 
+ '''
 
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -116,7 +116,7 @@ DATABASES = {
  # postgresql
  # pip install psycopg2-binary
 
-'''
+
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
@@ -129,6 +129,7 @@ DATABASES = {
  } 
  
  
+ '''
 
 import dj_database_url
 import os
@@ -138,7 +139,6 @@ DATABASES = {
         default=os.environ.get('postgresql://django_crud_db_3v3d_user:jMaKecbHvBwhdAdULnXhOIwoC79B8MAm@dpg-d1t8jrbe5dus73bo5sdg-a/django_crud_db_3v3d')
     )
 }
- '''
 
 
 # Password validation
